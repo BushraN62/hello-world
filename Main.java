d6e7f68f4e41e1e2e0d81f33befb0d21e27d6e41
BushraN62 <69646435+BushraN62@users.noreply.github.com>
2020-08-13 11:02:39 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Bushra Naveed");
+		System.out.println("I like a lot of sports especially Badminton");
+		System.out.println("I want to go to SJSU");
+		System.out.println("I am 16 years old");
+		System.out.println("I also like reading books");
+	}
+}
